eda12c1faea084db9531100f32f26043736b3157
datodvp <gaguadato98@gmail.com>
2023-03-17 15:59:20 +0400

    added navigation on user click from home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from 'react';
 import getUsers from '../../api/getUsers';
 import { infiniteScroll } from '../../utils';
 import LoadingImg from '../../images/loading.svg';
+import { useNavigate } from 'react-router-dom';
 import './styles.scss';
 
 const Home = () => {
   const [usersList, setUsersList] = useState([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     getUsers(page, 20).then((data) => {
@@ -29,13 +31,23 @@ const Home = () => {
       });
   }, []);
 
+  const handleClick = (userId) => {
+    navigate(`user/${userId}`);
+  };
+
   return (
     <>
       <div className="list">
         {usersList.length
           ? usersList.map((user) => {
               return (
-                <div key={user.id} className="list-item">
+                <div
+                  key={user.id}
+                  className="list-item"
+                  onClick={() => {
+                    handleClick(user.id);
+                  }}
+                >
                   <div className="content">
                     <img src={`${user.imageUrl}?v=${user.id}`} alt="user"></img>
                     <div className="description">
